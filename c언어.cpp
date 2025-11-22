584a63758eafcc2b36d06fa0aefa1191cf350e67
정현우 <harace7777@gmail.com>
2025-11-22 11:49:51 +0900

    Add files via upload

---
+++ c언어.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,mul;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    mul = a*b;
+    printf("Multiple:%d\n", mul);
+    return 0;
+}
